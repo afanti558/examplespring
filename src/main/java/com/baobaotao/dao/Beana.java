e133c0e32bdbaf0454c37c5660fa841080c8ca01
xflin <xflin@raipeng.com>
2015-03-31 08:39:36 +0800

    IoC中bean注入

---
+++ src/main/java/com/baobaotao/dao/Beana.java
@@ -0,0 +1,19 @@
+package com.baobaotao.dao;
+
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * Created by xiaofan on 2015/3/30.
+ */
+public class Beana {
+    private Map map;
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+}
